f58531473f37a01dc1febe4d515100d53fc2d1b5
ogius <kowalewski.olgierd@gmail.com>
2023-11-10 08:50:56 +0100

    Ended demonstration version

---
+++ src/include.h
@@ -0,0 +1,4 @@
+#include "utils/include.h"          // first
+#include "primaryClasses/include.h" // .
+#include "objects/include.h"        // after "primaryClasses/."
+#include "perFrame/include.h"       // last
